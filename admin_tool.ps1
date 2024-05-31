1ad465fe8563a66f43c382e1375ec9ea25183f62
DianaSofiaOlano <1005967885@u.icesi.edu.co>
2024-05-30 21:06:27 -0500

    Menu added for powershell script

---
+++ admin_tool.ps1
@@ -0,0 +1,16 @@
+﻿function Show-Menu
+{
+     param (
+           [string]$Title = ‘Herramienta de Administración del Data Center’
+     )
+
+     cls
+     Write-Host “================ $Title ================”
+    
+     Write-Host "1. Top 5 procesos por CPU"
+     Write-Host "2. Filesystems y discos"
+     Write-Host "3. Archivo más grande"
+     Write-Host "4. Memoria y swap"
+     Write-Host "5. Conexiones de red"
+     Write-Host "0. Salir"
+}
