702f4bf9214a16e48b3c5ff14e7ce006aaee15f1
DianaSofiaOlano <1005967885@u.icesi.edu.co>
2024-05-31 20:35:31 -0500

    Get-Top-CPU-Processes and Get-FileSystems-Disk functions added to powershell script

--- admin_tool.ps1
+++ admin_tool.ps1
@@ -1,13 +1,13 @@
 ﻿function Show-Menu
 {
      param (
-           [string]$Title = 'Herramienta de Administración del Data Center'
+        [string]$Title = 'Administración del Data Center'
      )
 
      cls
-     Write-Host "================================================="
+     Write-Host "=================================="
      Write-Host "  $Title  "
-     Write-Host "================================================="
+     Write-Host "=================================="
      Write-Host ""
      Write-Host "1. Mostrar los cinco procesos de mayor consumo de CPU"
      Write-Host "2. Mostrar discos y sus tamaños, incluyendo espacio libre"
@@ -18,6 +18,25 @@
      Write-Host ""
 }
 
+function Get-Top-CPU-Processes
+{
+   Get-Process | Sort-Object -Property CPU -desc | Select-Object -First 5 | Format-Table -Property Id, ProcessName, CPU
+}
+
+function Get-FileSystems-Disk {
+    param (
+        $ComputerName = 'localhost'
+    )
+
+    Get-WmiObject -Class Win32_logicaldisk -ComputerName $ComputerName |
+    Where-Object {$_.DriveType -eq 2 -or $_.DriveType -eq 3} |
+    Select-Object -Property DeviceID, @{n='Tamaño (Bytes)'; e={$_.Size}}, 
+    @{n='Espacio Libre (Bytes)'; e={$_.FreeSpace}} | Format-Table
+}
+
+
+
+
 do {
     Show-Menu
     $option = Read-Host "Seleccione una opción"
@@ -27,37 +46,33 @@ do {
         1 {
             cls
             Write-Host "=========Top 5 procesos por CPU========="
-            Write-Host ""
+            Get-Top-CPU-Processes #-AutoSize
             break
         }
         2 {
             cls
             Write-Host "=========Filesystems y discos========="
-            Write-Host ""
+            Get-FileSystems-Disk #-AutoSize
             break
         }
         3 {
             cls
             Write-Host "=========Archivo más grande========="
-            Write-Host ""
             break
         }
         4 {
             cls
             Write-Host "=========Memoria y swap========="
-            Write-Host ""
             break
         }
         5 {
             cls
             Write-Host "=========Conexiones de red========="
-            Write-Host ""
             break
         }
         0 {
             cls
             Write-Host "=========Saliendo========="
-            Write-Host ""
             break
         }
         default {
@@ -69,4 +84,5 @@ do {
     Write-Host ""
     pause
     Write-Host ""
+    cls
 } while ($option -ne 0)
