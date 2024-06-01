76efa9ab070806109c5c6b32384c38f480c1e007
DianaSofiaOlano <1005967885@u.icesi.edu.co>
2024-06-01 18:28:59 -0500

    Script documentation added to powershell script

--- admin_tool.ps1
+++ admin_tool.ps1
@@ -1,4 +1,15 @@
-﻿function Show-Menu {
+﻿
+<#
+.SYNOPSIS
+Muestra un menú de opciones para la administración del data center.
+
+.DESCRIPTION
+Este script muestra un menú interactivo que permite al usuario seleccionar diferentes opciones para administrar el data center.
+
+.PARAMETER Title
+Título para el menú. Por defecto es 'ADMINISTRACIÓN DEL DATA CENTER'.
+#>
+function Show-Menu {
      param (
         [string]$Title = 'ADMINISTRACIÓN DEL DATA CENTER'
      )
@@ -17,11 +28,37 @@
      Write-Host ""
 }
 
+<#
+.SYNOPSIS
+Obtiene los cinco procesos que más CPU están consumiendo en ese momento.
+
+.DESCRIPTION
+Get-TopCPUProcesses utiliza el cmdlet Get-Process para obtener una lista de procesos en el sistema, 
+luego los ordena según el uso de CPU en orden descendente y selecciona los primeros cinco procesos. 
+A continuación, muestra información detallada sobre estos procesos, incluyendo el ID del proceso, 
+el nombre del proceso y el porcentaje de uso de CPU.
+#>
 function Get-TopCPUProcesses {
    Get-Process | Sort-Object -Property CPU -desc | Select-Object -First 5 | 
    Select-Object -Property Id, ProcessName, CPU
 }
 
+<#
+.SYNOPSIS
+Obtiene información sobre los filesystems o discos conectados a la máquina.
+
+.DESCRIPTION
+Get-FileSystemsDisk utiliza WMI para consultar las instancias Win32_LogicalDisk en una máquina especificada por el parámetro ComputerName. 
+Filtra los resultados para incluir solo los discos duros y unidades de red (DriveType 2 o 3). 
+Luego, muestra el ID del dispositivo, el tamaño total del disco y el espacio libre en bytes.
+
+.PARAMETER ComputerName
+Nombre del computador del cual se desea obtener la información de los discos. 
+El valor predeterminado es 'localhost'.
+
+.EXAMPLE
+Get-FileSystemsDisk -ComputerName server1
+#>
 function Get-FileSystemsDisk {
     param (
         $ComputerName = 'localhost'
@@ -33,6 +70,21 @@ function Get-FileSystemsDisk {
     @{n='Espacio Libre(Bytes)'; e={$_.FreeSpace / 1 -as [int64]}}
 }
 
+<#
+.SYNOPSIS
+Obtiene el nombre y el tamaño del archivo más grande almacenado en un disco o filesystem.
+
+.DESCRIPTION
+Get-LargestFile busca en un directorio especificado y sus subdirectorios para encontrar el archivo más grande. 
+Si el directorio especificado no existe o no contiene archivos, muestra un mensaje de error.
+Muestra el nombre del archivo, el tamaño en bytes y la ruta completa.
+
+.PARAMETER Path
+Ruta del directorio en el cual se desea buscar el archivo más grande. Este parámetro es obligatorio.
+
+.EXAMPLE
+Get-LargestFile -Path C:\Users\Documents
+#>
 function Get-LargestFile {
     [CmdletBinding()]
     param(
@@ -64,6 +116,16 @@ function Get-LargestFile {
     }
 }
 
+
+<#
+.SYNOPSIS
+Obtiene la cantidad de memoria libre y la cantidad de espacio de swap en uso.
+
+.DESCRIPTION
+Get-MemorySwapInfo utiliza WMI para obtener información sobre la memoria física libre y el uso del espacio de swap en el sistema. 
+Calcula tanto la cantidad como el porcentaje de memoria libre y espacio de swap en uso, 
+y devuelve esta información en un objeto personalizado.
+#>
 function Get-MemorySwapInfo {
     $SystemInformation = Get-WmiObject -Class Win32_OperatingSystem
     $SwapInformation = Get-WmiObject -Class Win32_PageFileUsage
@@ -86,11 +148,29 @@ function Get-MemorySwapInfo {
     return $MemoryInfo
 }
 
+
+<#
+.SYNOPSIS
+Obtiene el número de conexiones de red activas actualmente en estado ESTABLISHED.
+
+.DESCRIPTION
+Get-NetworkConnections utiliza el cmdlet Get-NetTCPConnection para obtener todas las conexiones TCP en el sistema 
+y filtra aquellas que están en estado ESTABLISHED. Luego, cuenta el número de estas conexiones y devuelve el resultado.
+#>
 function Get-NetworkConnections {
     Get-NetTCPConnection | Where-Object {$_.State -eq "Established"} | Measure-Object | 
     Select-Object -Property @{n="Número de Conexiones con estado ESTABLISHED"; e={$_.Count}}
 }
 
+<#
+.SYNOPSIS
+Proporciona un menú interactivo para ejecutar varios scripts de administración del data center.
+
+.DESCRIPTION
+Este script muestra un menú de opciones para realizar diferentes tareas de administración del data center.
+El usuario puede seleccionar una opción y el script ejecutará la tarea correspondiente. 
+El menú se repetirá hasta que el usuario seleccione la opción de salir.
+#>
 do {
     Show-Menu
     $option = Read-Host "Seleccione una opción"
